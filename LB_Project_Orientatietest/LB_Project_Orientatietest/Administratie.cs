7e07419088fd93a383957d1f2694e0d119816122
Lars <l.blom@student.fontys.nl>
2015-11-04 17:18:24 +0100

    Opgave 3 UNITTEST NOT DONE

--- LB_Project_Orientatietest/LB_Project_Orientatietest/Administratie.cs
+++ LB_Project_Orientatietest/LB_Project_Orientatietest/Administratie.cs
@@ -41,5 +41,58 @@ namespace LB_Project_Orientatietest
         {
             verkopen.Add(verkoop);
         }
+
+        public List<IInkomsten> Overzicht(DateTime van, DateTime tot)
+        {
+            List<Verhuur> sortedList = verhuringen;
+            sortedList.Sort((x, y) => x.Tijdstip.CompareTo(y.Tijdstip));
+            sortedList.Reverse();
+            List<IInkomsten> LijstVanTot = new List<IInkomsten>();
+            foreach (Verhuur v in sortedList)
+            {
+                if ((v.Tijdstip < tot) && (van < v.Tijdstip))
+                {
+                    LijstVanTot.Add((IInkomsten)v);
+                }
+            }
+            return LijstVanTot;
+        }
+        public List<IInkomsten> Overzicht(BTWTarief tarief)
+        {
+            List<IInkomsten> LijstTarieven = new List<IInkomsten>();
+            List<Verkoop> sortedList = verkopen;
+
+            switch (tarief)
+            {
+                case BTWTarief.Ongespecificeerd:
+                    sortedList.Sort((x, y) => x.Tijdstip.CompareTo(y.Tijdstip));
+                    sortedList.Reverse();
+                    foreach (Verkoop v in sortedList)
+                    {
+                        LijstTarieven.Add((IInkomsten)v);
+                    }
+                    goto default;
+                case BTWTarief.Laag:
+                    sortedList.Sort((x, y) => x.Tijdstip.CompareTo(y.Tijdstip));
+                    sortedList.Reverse();
+                    foreach (Verkoop v in sortedList)
+                    {
+                        if (v.BTWTarief == BTWTarief.Laag)
+                            LijstTarieven.Add((IInkomsten)v);
+                    }
+                    goto default;
+                case BTWTarief.Hoog:
+                    sortedList.Sort((x, y) => x.Tijdstip.CompareTo(y.Tijdstip));
+                    sortedList.Reverse();
+                    foreach (Verkoop v in sortedList)
+                    {
+                        if (v.BTWTarief == BTWTarief.Hoog)
+                            LijstTarieven.Add((IInkomsten)v);
+                    }
+                    goto default;
+                default:
+                    return LijstTarieven;
+            }
+        }
     }
 }
